d022bbfaedd462e051259592a98700b8eb445a9a
KoKoSeKPolska <116282831+KoKoSeKPolska@users.noreply.github.com>
2022-12-28 18:31:05 +0100

    Add files via upload

---
+++ bank alpha 1.1/bank/uzytkownicy.sql
@@ -0,0 +1,74 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Czas generowania: 28 Gru 2022, 18:30
+-- Wersja serwera: 10.4.27-MariaDB
+-- Wersja PHP: 8.1.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Baza danych: `uzytkownicy`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `uzytkownicy`
+--
+
+CREATE TABLE `uzytkownicy` (
+  `id` int(11) NOT NULL,
+  `login` varchar(28) NOT NULL,
+  `pin` int(11) NOT NULL,
+  `money` float NOT NULL,
+  `log_date` datetime NOT NULL,
+  `log_date_close` datetime NOT NULL,
+  `ban` bit(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_polish_ci;
+
+--
+-- Zrzut danych tabeli `uzytkownicy`
+--
+
+INSERT INTO `uzytkownicy` (`id`, `login`, `pin`, `money`, `log_date`, `log_date_close`, `ban`) VALUES
+(1, 'Marek Rusin', 1234, 10000, '2022-12-28 18:23:21', '2022-12-28 19:22:22', b'0'),
+(2, 'Ksiądz Mateusz', 2137, 2000, '2022-12-28 18:23:21', '2022-12-26 17:25:29', b'1'),
+(3, 'Elon Musk', 1337, 1000000, '2022-12-28 18:23:21', '2022-12-28 19:22:04', b'0'),
+(4, 'Marcin Najman', 13421, 100, '2022-12-28 18:23:21', '2022-12-27 04:18:07', b'1'),
+(5, 'Przemysław Łuniewski', 6060, 5000, '2022-12-28 18:23:21', '2022-12-27 04:10:32', b'1');
+
+--
+-- Indeksy dla zrzutów tabel
+--
+
+--
+-- Indeksy dla tabeli `uzytkownicy`
+--
+ALTER TABLE `uzytkownicy`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT dla zrzuconych tabel
+--
+
+--
+-- AUTO_INCREMENT dla tabeli `uzytkownicy`
+--
+ALTER TABLE `uzytkownicy`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
