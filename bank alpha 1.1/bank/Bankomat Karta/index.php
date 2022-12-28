e6a63f457dee8aaf51625e8741fc174beb494ada
KoKoSeKPolska <116282831+KoKoSeKPolska@users.noreply.github.com>
2022-12-28 18:26:27 +0100

    mechanizm bana na godzine
    po wpisaniu złego loginu uyztkownik dostaje bana na godzine

---
+++ bank alpha 1.1/bank/Bankomat Karta/index.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="stylekarta.css">
+    <script src="scriptkarta.js"></script>
+    <title>Bankomat</title>
+</head>
+<body onload="kartaobiekt()">
+    <div class="div1">
+        <h1>Wybierz swoją kartę debetową!</h1>
+        <form action = "../Bankomat intrfejs/index.php" class ="btn1" id="btn1" method="post">    
+            <div class="btn_inside">
+            <input type="submit" class="btnws" name="login" value="Marcin Najman">
+                <div class="id1" id="id1">
+                    <div class="imie">Marcin Najman</div> 
+                </div>
+                <div class="name">
+                    <div class="pasek">
+
+                    </div>
+
+                </div>
+            </div>
+        </form>
+        <form action = "../Bankomat intrfejs/index.php" class ="btn1" id="btn2" method="post">
+        <div class="btn_inside">
+            <input type="submit" class="btnws" name="login" value="Ksiądz Mateusz">
+                <div class="id1" id="id2">
+                    <div class="imie">Ksiądz Mateusz</div> 
+                </div>
+                <div class="name">
+                    <div class="pasek">
+
+                    </div>
+
+                </div>
+            </div>
+    
+        </form>
+        <form action = "../Bankomat intrfejs/index.php" class ="btn1" id="btn3" method="post">
+        <div class="btn_inside">
+            <input type="submit" class="btnws" name="login" value="Elon Musk">
+                <div class="id1" id="id3">
+                    <div class="imie">Elon Musk</div> 
+                </div>
+                <div class="name">
+                    <div class="pasek">
+
+                    </div>
+
+                </div>
+            </div>
+    
+        </form>
+        <form action = "../Bankomat intrfejs/index.php" class ="btn1" id="btn4" method="post">
+        <div class="btn_inside">
+            <input type="submit" class="btnws" name="login" value="Marek Rusin">
+                <div class="id1" id="id4">
+                    <div class="imie">Marek Rusin</div> 
+                </div>
+                <div class="name">
+                    <div class="pasek">
+
+                    </div>
+
+                </div>
+            </div>
+    
+        </form>
+        <form action = "../Bankomat intrfejs/index.php" class ="btn1" id="btn5" method="post">
+        <div class="btn_inside">
+            <input type="submit" class="btnws" name="login" value="Przemysław Łuniewski">
+                <div class="id1" id="id5">
+                    <div class="imie">Przemysław Łuniewski</div> 
+                </div>
+                <div class="name">
+                    <div class="pasek">
+
+                    </div>
+
+                </div>
+            </div>
+    
+        </form>
+    </div>
+
+</body>
+</html>
